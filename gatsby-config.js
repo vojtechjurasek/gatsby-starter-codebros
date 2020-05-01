67739435600ed29581e2c86d96d8ac079c7c22de
Vojtech Jurasek <jurasek@ctyrimedia.cz>
2020-05-01 10:26:42 -0500

    Add gatsby-plugin-netlify package

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,8 +28,9 @@ module.exports = {
         icon: 'src/images/icon.png', // This path is relative to the root of the site.
       },
     },
+    'gatsby-plugin-netlify',
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    // 'gatsby-plugin-offline',
   ],
-};
+}
